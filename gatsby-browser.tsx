77d086974bf52f6d387654ae16877584d73854d3
Tomasz Duda <tomasz.duda@MBP-OK-116.local>
2022-11-05 14:13:14 +0100

    added normalize

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,9 @@
 import { GatsbyBrowser } from "gatsby";
 import React from "react";
+
 import Layout from './src/components/Layout';
+
+import 'normalize.css';
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element, props }) => {
   return (
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,6 +1,9 @@
 import { GatsbySSR } from "gatsby";
 import React from "react";
+
 import Layout from './src/components/Layout';
+
+import 'normalize.css';
 
 export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element, props }) => {
   return (
