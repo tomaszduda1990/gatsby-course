f0a2696bee6c641397862ae5c5115ddda23b28a0
Tomasz Duda <tomasz.duda@MBP-OK-116.local>
2022-11-05 13:04:52 +0100

    started with stuff

---
+++ src/pages/beers.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function BeersPage(): JSX.Element {
+  return <div>Beers</div>;
+}
---
+++ src/pages/pizzas.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PizzasPage(): JSX.Element {
+  return <div>Pizzas</div>;
+}
