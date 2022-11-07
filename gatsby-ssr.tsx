fc8335412f755a776245077094f677febb18b148
Tomasz Duda <tomaszduda1990@users.noreply.github.com>
2022-11-07 20:14:51 +0100

    Initial steps (#1)
    * added gatsby ssr
    
    * added normalize
    
    * css corrections
    
    Co-authored-by: Tomasz Duda <tomasz.duda@MBP-OK-116.local>

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,11 +1,11 @@
-import { GatsbyBrowser } from "gatsby";
+import { GatsbySSR } from "gatsby";
 import React from "react";
 
 import Layout from './src/components/Layout';
 
 import 'normalize.css';
 
-export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({ element, props }) => {
+export const wrapPageElement: GatsbySSR["wrapPageElement"] = ({ element, props }) => {
   return (
     <Layout {...props}>
       {element}
