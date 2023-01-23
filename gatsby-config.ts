0f6dc17e3380983d92d23f2c7d2ace13b78c9c28
Tomasz Duda <tomasz.duda@MBP-OK-116.local>
2023-01-23 20:14:28 +0100

    config with graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,14 +12,27 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [ "gatsby-plugin-styled-components", {
+  plugins: [ 
+  "gatsby-plugin-styled-components",    
+  'gatsby-plugin-image',
+  {
   resolve: 'gatsby-plugin-react-svg',
   options: {
     rule: {
       include: `/svg/`
     }
   }
-}]
+}, 
+{
+  resolve: 'gatsby-source-sanity',
+  options: {
+    projectId: '7lrtv4r5',
+    dataset: 'production',
+    watchMode: true,
+    token: process.env.SANITY_TOKEN
+  }
+}
+]
 };
 
 export default config;
